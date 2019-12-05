995f006d00df0953b262a8b44641fbff357a411e
Rushi James Macwan <rushi.macwan@colorado.edu>
2019-12-05 05:45:01 -0700

    Fixed script error

--- rfid-test/Read.py
+++ rfid-test/Read.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python
 
 # Source - https://pimylifeup.com/raspberry-pi-rfid-rc522/
+# For RPI test connections, please see the source document referenced above. Thanks.
 
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 
 reader = SimpleMFRC522()
-echo "RFID Sensor Connected\n"
-echo "RFID Sensor waiting for user registration...\n"
+print("RFID Sensor Connected\n")
+print("RFID Sensor waiting for user registration...\n")
+print("Please place your RFID card on the sensor sense-pad...\n")
 
 try:
-        id, text = reader.read()
-		echo "Printing user unique ID\n"
-        print(id)
-		echo "Printing user name\n" 
-        print(text)
+	id, text = reader.read()
+	print("Printing user unique ID\n")
+	print(id)
+	print("\n")
+	print("Printing user name\n")
+	print(text)
+	print("\n")
 		
 finally:
-        GPIO.cleanup()
-		echo "Stopped interaction with RFID Sensor\n"
+	GPIO.cleanup()
+	print("Stopped interaction with RFID Sensor\n")
