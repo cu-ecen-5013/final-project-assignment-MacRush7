b5b0ef21d220bed3b8820c391c847035c6ac0749
sast7580 <46766721+sast7580@users.noreply.github.com>
2019-12-04 23:30:52 -0700

    Update buzzerModule.h

--- buzzer-driver/buzzerModule.h
+++ buzzer-driver/buzzerModule.h
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <syslog.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
 
-int buzzerFile;
-
-void buzzer();
+void buzzer(int passedFile);
