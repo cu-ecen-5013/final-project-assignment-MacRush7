00684e32d63a788c180cb8c96b2864e226dd3044
sast7580 <46766721+sast7580@users.noreply.github.com>
2019-12-05 23:36:28 -0700

    fully working... may want to move file origin from dev

--- buzzer-driver/buzzerModule.c
+++ buzzer-driver/buzzerModule.c
@@ -1,60 +1,60 @@
 void buzzer(int passedFile)
 {
-	syslog(LOG_INFO, "Buzzer alerts detected");
-	
-	char *noAlarm = "N";
-	int i = 0;
+	char *noAlarm = "0";
+	int i = 0, gpioFile;
 	
 	// set GPIO header 9, pin 12 to output = pin 60 (32*1 + 28)
-	int file = open("/sys/class/gpio/gpio60/value", O_WRONLY);
-	if(file == -1)
-	{
-		printf("buzzer file didnt open\n");
-		syslog(LOG_INFO, "buzzer file didn't open");
-	}
-	else
-		printf("buzzer file opened\n");	
+	gpioFile = open("/sys/class/gpio/gpio60/value", O_WRONLY);
+	if(gpioFile == -1)
+		syslog(LOG_ERR, "buzzer file didn't open");
 	
 	// turn on buzzer for three seconds and turn off for 1 second... repeat
 	for(i = 0; i < 3; i++)
 	{
-		write(file, "1", 1);
+		lseek(gpioFile, 0, SEEK_SET);
+		write(gpioFile, "1", 1);
 		usleep(3000000);
-		write(file, "0", 1);
+		lseek(gpioFile, 0, SEEK_SET);
+		write(gpioFile, "0", 1);
 		usleep(1000000);
 	}
 	
 	// reset file
-	int ret = write(passedFile, noAlarm, strlen(noAlarm));
+	lseek(passedFile, 0, SEEK_SET);
+	int ret = write(passedFile, noAlarm, 1);
 	if(ret == -1)
-		syslog(LOG_ERROR, "file was not reset to no");
+		syslog(LOG_ERR, "file was not reset to 0");
 	
-	close(file);
+	close(gpioFile);
 }
 
 int main()
 {
-	char *buffer = "0";
-	char *yesAlarm = "Y";
+	char *buffer;
+	char *fileStarter = "0";
 	int ret = 3, i = 0, buzzerFile;
 	
 	// setup file
 	if((buzzerFile = open("/dev/buzzerAlarm", O_RDWR | O_CREAT, 0666)) == -1)
 	{
-		syslog(LOG_ERROR, "file was not found or opened");
+		syslog(LOG_ERR, " buzzer file was not found or opened");
 		return -1;
 	}
+	
+	lseek(buzzerFile, 0, SEEK_SET);
+	write(buzzerFile, fileStarter, 1);
 	
 	while(1)
 	{
 		// check file for alarm
-		int ret = read(buzzerFile, buffer, strlen(yesAlarm));
+		lseek(buzzerFile, 0, SEEK_SET);
+		ret = read(buzzerFile, buffer, 1);
 		if(ret == 0)
-			syslog(LOG_ERROR, "nothing in file to read");
+			syslog(LOG_ERR, "nothing in buzzer file to read");
 
-		ret = strcmp(buffer, yesAlarm);
-		// file contains "Y"
-		if(ret == 0)
+		ret = atoi(buffer);
+		// file contains "1"
+		if(ret == 1)
 			buzzer(buzzerFile);
 	}	
 
