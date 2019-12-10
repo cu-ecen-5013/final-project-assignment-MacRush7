1c18de2a8d145c824dfecc0fb0f1c04f98b1948a
Sarah Stephany <sast7580@colorado.edu>
2019-12-10 12:10:19 -0700

    updated terminal interface

--- terminal.sh
+++ terminal.sh
@@ -8,16 +8,13 @@ set -e
 HEIGHT=15
 WIDTH=50
 CHOICE_HEIGHT=4
-OPTIONS=(1 "Fingerprint Sensor"
-	2 "RFID Sensor"
-	3 "GSM Module")
 
 # make dialog box
-CHOICE=$(dialog --title "Welcome! What would you like to run?" \
+CHOICE=${dialog --title "Welcome! How Would you Like to Verify your Access?" \
 	--menu "Please Select a Program:" \
 	$HEIGHT $WIDTH $CHOICE_HEIGHT \
-	"${OPTIONS[@]}" \
-	2>&1 >/dev/tty)
+	1 "Fingerprint Sensor" 2 "RFID Sensor" 3 "GSM Module" \
+	2>&1 >/dev/tty}
 
 clear
 case $CHOICE in
