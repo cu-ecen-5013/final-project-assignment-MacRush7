fe395ada68fd4048de4533aa77179c0673ccd1ad
Sarah Stephany <sast7580@colorado.edu>
2019-12-11 13:18:43 -0700

    added more python libs

--- rfid-test/rfidtest.sh
+++ rfid-test/rfidtest.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-sudo python3 Write.py && echo "Unique RFID card added...\n"
-
+python3 Write.py && echo "Unique RFID card added...\n"
 sleep 3
-
-sudo python3 Read.py && echo "User detected...\n"
-
+python3 Read.py && echo "User detected...\n"
 echo "RFID TEST SUCCESSFUL"
